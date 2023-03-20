def42a57d0cc6cef6147014b8d0480c7d4b69376
simonanderson16 <uhx8bu@virginia.edu>
2023-03-20 18:42:00 -0400

    refactored to track previous board states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
 
-export default function Board() {
+export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
 
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
+
+function Board({xIsNext, squares, onPlay}) {
   function handleClick(i) {
     if(squares[i] || calculateWinner(squares)) {
       return;
@@ -14,8 +33,7 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
