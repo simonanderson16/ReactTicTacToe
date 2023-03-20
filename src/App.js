4a55cba9bcf2fc0de0bf360e48188359871a4ace
simonanderson16 <uhx8bu@virginia.edu>
2023-03-19 22:43:01 -0400

    created function to determine if there has been a winner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-      const nextSuares = squares.slice();
-      nextSuares[i] = "X";
-      setSquares(nextSuares);
+    if(squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
@@ -36,4 +45,24 @@ function Square({value, onSquareClick}) {
       {value}
     </button>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
