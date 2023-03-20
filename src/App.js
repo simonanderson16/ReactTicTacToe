3dd6345f46df5d27206a5f58d4b79c576135f8e9
simonanderson16 <uhx8bu@virginia.edu>
2023-03-19 22:46:30 -0400

    added code to display status (next player or winner)

--- src/App.js
+++ src/App.js
@@ -18,8 +18,17 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <div>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
